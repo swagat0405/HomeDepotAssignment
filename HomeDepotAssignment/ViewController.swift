347ad7f2d609716684984cecf26c7d7faf5e8ca4
Swagat Mishra <swagat_mishra1990@hotmail.com>
2018-05-02 17:43:22 -0400

    Initial Commit

---
+++ HomeDepotAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeDepotAssignment
+//
+//  Created by Swagat Mishra on 5/2/18.
+//  Copyright © 2018 Swagat Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
